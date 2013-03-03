7bd013268a6fa2099878f2405a77412a9c978d3b
Joshua Bussdieker <jbussdieker@gmail.com>
2013-03-03 00:55:32 -0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Httpbench::Application.config.secret_token = '7b1eed3b5bd8fa3e2bbb7f9a67079a7028a5e643d68f5fa7629320b4c5aa370bc88b152147456fee7a1d7cf3ffd9f7db228e64bd7ef4ecbad1447948f8b3a518'
